c9317625d0cccc114eb6de59ecc9ccf304237cbe
shunya1135 <sstana1132@gmail.com>
2026-01-30 10:55:47 +0900

    feat: feedback repositoryを実装

---
+++ backend/internal/domain/repository/feedback_repository.go
@@ -0,0 +1,10 @@
+package repository
+
+import (
+	"abema-discovery/backend/internal/domain/entity"
+	"context"
+)
+
+type FeedbackRepository interface {
+	Save(ctx context.Context, fb *entity.Feedback)
+}
