a31e3c6974e743687ba230cae937c562f75f7cbd
shunya1135 <sstana1132@gmail.com>
2026-01-30 09:36:17 +0900

    faet: mainを実装

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"abema-discovery/backend/internal/adapter/handler"
+	"abema-discovery/backend/internal/adapter/repository"
 	"abema-discovery/backend/internal/infrastructure/database"
+	"abema-discovery/backend/internal/infrastructure/router"
+	"abema-discovery/backend/internal/usecase"
 	"log"
+	"net/http"
 )
 
 func main() {
+	// 1.DB接続
 	db, err := database.NewMySQLConnection(database.Config{
 		Host:     "127.0.0.1",
 		Port:     "3306",
@@ -20,5 +26,19 @@ func main() {
 
 	defer db.Close()
 
-	log.Printf("DB接続成功！")
+	// 2.Repository(DBを渡す)
+	repo := repository.NewSQLConnectRepository(db)
+
+	// 3.Usecase(Repositoryを渡す)
+	uc := usecase.NewHiddenGemUsecase(repo)
+
+	// 4.Handler(Usecaseを渡す)
+	h := handler.NewHiddenGemHandler(uc)
+
+	// 5.Router(Handlerを渡す)
+	r := router.NewRouter(h)
+
+	// 6.サーバー起動
+	log.Println("サーバー起動：http://localhost:8080")
+	http.ListenAndServe(":8080", r)
 }
